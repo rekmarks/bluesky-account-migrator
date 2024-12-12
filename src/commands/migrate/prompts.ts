6dc0df3eb8cb58a5f41b03c35a166abc31b21251
Erik Marks <25517051+rekmarks@users.noreply.github.com>
2024-12-12 15:03:40 -0800

    refactor: Trim results from inquirer

--- src/commands/migrate/prompts.ts
+++ src/commands/migrate/prompts.ts
@@ -1,11 +1,12 @@
 import { input as _input, password as _password } from '@inquirer/prompts';
 
-const wrapPrompt = <T extends (...args: any[]) => Promise<unknown>>(
+const wrapPrompt = <T extends (...args: any[]) => Promise<string>>(
   prompt: T,
 ) => {
   return async (...args: Parameters<T>) => {
     try {
-      return await prompt(...args);
+      const result = await prompt(...args);
+      return result.trim();
     } catch (error) {
       if (error instanceof Error && error.name === 'ExitPromptError') {
         // The user exited the prompt. Wait for the next tick to avoid
