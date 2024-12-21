f0e86b8bd4b620b0100737f5f7cc189b79d91506
Erik Marks <25517051+rekmarks@users.noreply.github.com>
2024-12-21 04:10:22 -0800

    refactor: Tweak usage string

--- src/cli.ts
+++ src/cli.ts
@@ -4,11 +4,16 @@ import { hideBin } from 'yargs/helpers';
 import type { Commands } from './commands/index.js';
 import packageJson from 'bluesky-account-migrator/package.json' assert { type: 'json' };
 
+const usageDescription = `
+Usage:
+  $0 [migrate] [options]
+  bam [migrate] [options]`;
+
 export async function cli(argv: string[], commands: Commands) {
   await yargs(hideBin(argv))
     .scriptName('bluesky-account-migrator')
     .strict()
-    .usage('Usage:\n  $0 [migrate] [options]\n  bam [migrate] [options]')
+    .usage(usageDescription)
     .version(packageJson.version)
     .help()
     .showHelpOnFail(false)
