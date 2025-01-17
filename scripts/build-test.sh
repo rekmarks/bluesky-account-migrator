b4c5aed801f449b1cb363d60d7c6e990d8d93193
Erik Marks <25517051+rekmarks@users.noreply.github.com>
2025-01-16 21:51:32 -0800

    fix script

--- scripts/build-test.sh
+++ scripts/build-test.sh
@@ -9,7 +9,9 @@ credentials_file="./dist/commands/migrate/credentials.mjs"
 
 pnpm esbuild ./test/mocks/credentials.ts --outfile="$credentials_file" "${args[@]}"
 # For some reason esbuild imports vi from vitest. This blows up, so we remove it.
-sed -i '' '/import.*vi.*from.*vitest.*/d' "$credentials_file"
+tmpfile=$(mktemp)
+sed '/import.*vi.*from.*vitest.*/d' "$credentials_file" > "$tmpfile"
+mv "$tmpfile" "$credentials_file"
 
 pnpm esbuild ./test/mocks/operations.ts --outfile=./dist/migration/operations/index.mjs "${args[@]}"
 
